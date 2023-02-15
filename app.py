753298562447b112066fd307fa8f093a9ce18f19
fabiokavlac <fabiokavlac@gmail.com>
2023-02-15 22:38:24 +0000

    Aula3.0

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask("_name_")
+SECRET_KEY = "pudim"
+app.config.from_object(__name__)
 
 #POSTS MOCK
 
@@ -19,6 +21,22 @@ posts = [
     }
 ]
 
+# USER MOCKS
+USERNAME = "admin"
+PASSWORD = "admin"
+
 @app.route('/')
 def exibir_entradas():
     return render_template("exibir_entradas.html", entradas=posts)
+
+@app.route('/login', methods=["GET", "POST"])
+def login(): 
+    erro = ""
+    if request.method == "POST":
+        if request.form['username'] == USERNAME and request.form['password'] == PASSWORD:
+            session['logado'] = True
+            return redirect (url_for('exibir_entradas'))
+        erro = "Usuário ou senha inválidos"
+        print(request.form['username'], request.form ['password'])
+        return "dados recebidos"
+    return render_template('login.html',  erro=erro)
